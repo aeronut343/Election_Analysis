0dea760fa2680ff3460db78f25b58321438fca63
aeronut343 <halex211@umn.edu>
2021-07-20 14:36:28 -0500

    Create Python_practice.py

---
+++ Python_practice.py
@@ -0,0 +1,7 @@
+counties = ["Arapahoe","Denver","Jefferson"]
+if counties[1] == 'Denver':
+    print(counties[1])
+
+voting_data = [{"county":"Arapahoe", "registered_voters": 422829},
+                {"county":"Denver", "registered_voters": 463353},
+                {"county":"Jefferson", "registered_voters": 432438}]
